b5580eb024a475d735dfdd795bbc2251f5a701fb
Zaki <menza.zm@gmail.com>
2023-09-25 18:15:27 +0600

    New place add static table view controller created

--- PlacesApp/MainViewController.swift
+++ PlacesApp/MainViewController.swift
@@ -39,5 +39,6 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 
 }
