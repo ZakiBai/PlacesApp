a74391eaa36d7b5d57b451c2d605c2066ee6e845
Zaki <menza.zm@gmail.com>
2023-10-05 17:58:17 +0600

    star templates as rectangle, UI part done

---
+++ PlacesApp/RatingControl.swift
@@ -0,0 +1,66 @@
+//
+//  RatingControl.swift
+//  PlacesApp
+//
+//  Created by Zaki on 05.10.2023.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+
+    var ratingButtons = [UIButton]()
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    override required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button tapped")
+    }
+    
+    var rating = 0
+    
+    func setupButtons() {
+        
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        
+        ratingButtons.removeAll()
+        
+        for _ in 1...starCount {
+            let button = UIButton()
+            button.backgroundColor = .red
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            button.addTarget(self, action: #selector(ratingButtonTapped(button: )), for: .touchUpInside)
+            
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+        }
+        
+    }
+}
