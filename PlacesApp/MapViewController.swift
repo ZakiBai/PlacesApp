d3da6e36898ce146e6549b13d40075bbd887c6b0
Zaki <menza.zm@gmail.com>
2023-10-14 17:31:32 +0600

    preparing map kit

---
+++ PlacesApp/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  PlacesApp
+//
+//  Created by Zaki on 14.10.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closeButtonTapped() {
+        dismiss(animated: true)
+    }
+}
